442ace6d5065d5a3a526cc068ade04c4021af1c8
anna41 <annasapuzhak94@gmail.com>
2016-05-07 13:01:13 +0300

    Some commit message

--- Tasks_for_kottans/Program.cs
+++ Tasks_for_kottans/Program.cs
@@ -15,7 +15,6 @@ namespace Tasks_for_kottans
             Console.WriteLine("Enter your credit card number:");
             string q = Console.ReadLine();
             GetCreditCardVendor(q);
-            Console.WriteLine(GetCreditCardVendor(q));
             if (def == true)
             {
                 if (IsCreditCardNumberValid(q))
@@ -24,9 +23,17 @@ namespace Tasks_for_kottans
                     Console.WriteLine("Your card number is valid");
                 }
                 else
-                    Console.WriteLine("Your card number is not valid");
-                Console.WriteLine("Next valid number:");
-                GenerateNextCreditCardNumber(q);
+                    Console.WriteLine("Your card number is not valid");               
+                if (GetCreditCardVendor(q) != "Unknown")
+                {
+                    if (GetCreditCardVendor(GenerateNextCreditCardNumber(q)) == GetCreditCardVendor(q))
+                    {
+                        Console.WriteLine("Next valid number:");
+                        Console.WriteLine(GenerateNextCreditCardNumber(q));
+                    }
+                    else
+                        Console.WriteLine("No more card numbers available for this vendor");
+                }
             }
             else
                 Console.WriteLine(GetCreditCardVendor(q));
@@ -63,44 +70,50 @@ namespace Tasks_for_kottans
         }
         static bool IsCreditCardNumberValid(string a)
         {
-            a = a.Replace(" ", string.Empty);
-            double[] array_for_numbers = new double[a.Length];
-            int start;
-            double sum = 0;
-            for (int i = 0; i < a.Length; i++)
+            if (GetCreditCardVendor(a) == "Unknown")
             {
-                array_for_numbers[i] = Double.Parse(a[i].ToString());
+                return false;
             }
-            if (a.Length % 2 == 0)
-                start = 0;
             else
-                start = 1;
-            for (int i = start; i < array_for_numbers.Length; i = i + 2)
             {
-                double temp = Convert.ToDouble(array_for_numbers[i]) * 2;
-                if (temp > 9)
+                a = a.Replace(" ", string.Empty);
+                double[] array_for_numbers = new double[a.Length];
+                int start;
+                double sum = 0;
+                for (int i = 0; i < a.Length; i++)
                 {
-                    array_for_numbers[i] = temp % 10 + Math.Truncate(temp / 10);
+                    array_for_numbers[i] = Double.Parse(a[i].ToString());
+                }
+                if (a.Length % 2 == 0)
+                    start = 0;
+                else
+                    start = 1;
+                for (int i = start; i < array_for_numbers.Length; i = i + 2)
+                {
+                    double temp = Convert.ToDouble(array_for_numbers[i]) * 2;
+                    if (temp > 9)
+                    {
+                        array_for_numbers[i] = temp % 10 + Math.Truncate(temp / 10);
+                    }
+                    else
+                        array_for_numbers[i] = temp;
+                }
+                for (int i = 0; i < array_for_numbers.Length; i++)
+                {
+                    sum += array_for_numbers[i];
+                }
+                if (sum % 10 == 0)
+                {
+                    return true;
                 }
                 else
-                    array_for_numbers[i] = temp;
-            }
-            for (int i = 0; i < array_for_numbers.Length; i++)
-            {
-                sum += array_for_numbers[i];
-            }
-            if (sum % 10 == 0)
-            {
-                return true;
-            }
-            else
-            {               
-                return false;
+                {
+                    return false;
+                }
             }
         }
         static string GenerateNextCreditCardNumber(string a)
-        {
-            
+        {           
             a = a.Replace(" ", string.Empty);                      
             string next_number = a;
             bool check = false;
@@ -109,9 +122,8 @@ namespace Tasks_for_kottans
                 long number = Int64.Parse(next_number);
                 number++;
                 next_number = number.ToString();
-                check = IsCreditCardNumberValid(next_number);
-            }
-            Console.WriteLine(next_number);
+                check = IsCreditCardNumberValid(next_number);                
+            }            
             return next_number;
         }
     }
